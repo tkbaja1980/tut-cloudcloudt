925078ffb047b2f4b4db54b332deda851f704144
pyscalp <pyscalp@gmail.com>
2025-07-20 19:33:49 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import type { Metadata } from 'next';
 import React from 'react';
 
@@ -75,7 +73,7 @@ const ServerIcon = ({ className }: { className?: string }) => (
 );
 
 const LanguageIcon = ({ className }: { className?: string }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5">
         <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C13.18 7.061 14.285 7.5 15.5 7.5c1.215 0 2.32-.439 3.334-1.136M9 21h.01M15 21h.01M18 21h.01M12 21h.01M9 21h.01M15 21h.01M18 21h.01M12 21h.01M9 3.75a.75.75 0 01.75-.75h3.5a.75.75 0 01.75.75V5.25a.75.75 0 01-.75.75h-3.5a.75.75 0 01-.75-.75V3.75z" />
     </svg>
 );
